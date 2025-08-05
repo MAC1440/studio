f83a8cca7cac831c72b472bbaa76a9c84379b41c
Ahmad Afzal <ahmad.afzal1440@gmail.com>
2025-08-05 07:35:19 +0000

    yeah that sounds way better. thanks. lets do thta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
+          themes={['light', 'dark', 'midnight', 'forest']}
         >
           <AuthProvider>
             {children}
