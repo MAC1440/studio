73bd5e46b860556eba7472b11d7c11ec2b269ab3
Ahmad Afzal <ahmad.afzal1440@gmail.com>
2025-08-05 06:13:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/client/project/[projectId]/page.tsx
+++ src/app/client/project/[projectId]/page.tsx
@@ -2,12 +2,12 @@
 import ClientProjectView from '@/components/client/ClientProjectView';
 
 // This is a Server Component. It can directly access params.
-export default function ClientProjectBoardPage({ params }: { params: { projectId: string } }) {
+export default function ClientProjectBoardPage({ params: { projectId } }: { params: { projectId: string } }) {
   return (
     <div className="flex flex-col h-full bg-background">
       <main className="flex-1 overflow-hidden">
         {/* We pass the projectId to the ClientProjectView component, which is a Client Component */}
-        <ClientProjectView projectId={params.projectId} />
+        <ClientProjectView projectId={projectId} />
       </main>
     </div>
   );
